65f3555490afaa4822a4b72ad509b822d00d9cc2
Starlight56-tech <panee55554@gmail.com>
2026-04-20 17:36:07 +0700

    eeeeeeeee

--- Assets/Script/MainMenuControl.cs
+++ Assets/Script/MainMenuControl.cs
@@ -1,19 +1,58 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenuControl : MonoBehaviour
 {
-    // ตัวแปรนี้เอาไว้บอก Unity ว่าหน้าต่าง Setting คืออันไหน
     public GameObject settingPanel;
+    public GameObject informationPanel;
+    public GameObject howToPlayPanel;
+    public AudioMixer myMixer;
 
-    // สั่งให้หน้าต่าง "แสดงผล"
+    // เชื่อมกับสคริปต์ ButtonSound โดยตรง
+    public ButtonSound buttonSoundScript;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // เรียกใช้ฟังก์ชัน PlayClick ที่คุณเขียนไว้ในอีกสคริปต์
+            if (buttonSoundScript != null)
+            {
+                buttonSoundScript.PlayClick();
+            }
+
+            if (settingPanel.activeSelf)
+            {
+                CloseSetting();
+            }
+            else
+            {
+                CloseAllPanels();
+                OpenSetting();
+            }
+        }
+    }
+
     public void OpenSetting()
     {
         settingPanel.SetActive(true);
     }
 
-    // สั่งให้หน้าต่าง "หายไป"
     public void CloseSetting()
     {
         settingPanel.SetActive(false);
+    }
+
+    public void CloseAllPanels()
+    {
+        if (settingPanel != null) settingPanel.SetActive(false);
+        if (informationPanel != null) informationPanel.SetActive(false);
+        if (howToPlayPanel != null) howToPlayPanel.SetActive(false);
+    }
+
+    public void SetVolume(float sliderValue)
+    {
+        if (sliderValue <= 0) sliderValue = 0.0001f;
+        myMixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
     }
 }
